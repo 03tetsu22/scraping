4ceccffe132c96f96b28e01d456e3ceb6f55a056
fesinx <shinmura@plumsa.co.jp>
2019-06-06 14:42:45 +0900

    体裁調整

--- get_table.py
+++ get_table.py
@@ -13,18 +13,18 @@ import sys
 ##################
 def extract_data(url):
     #<= webページへのリクエスト=>#
-    http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED",
-                            ca_certs=certifi.where())
+    http = urllib3.PoolManager(cert_reqs = "CERT_REQUIRED",
+                            ca_certs = certifi.where())
     request_result = http.request("GET",url)
-    soup   = BeautifulSoup(request_result.data,"html.parser")
+    soup = BeautifulSoup(request_result.data,"html.parser")
 
     #<= 名前と必殺技名の収集 =>#
     temp = soup.title.string.replace("の評価","")
     temp = temp.replace("- GameWith","")
     temp = temp.replace("【シンフォギアXD】","")
 
-    name_skill=temp.split("|")
-    name_skill=[i.strip(" ") for i in name_skill]
+    name_skill = temp.split("|")
+    name_skill = [i.strip(" ") for i in name_skill]
     # print(name_skill)
 
     #tableの抽出#
@@ -42,38 +42,38 @@ def extract_data(url):
     #<= 行の抽出=>#
     for tr in table:
         th_list = tr.find_all("th")
-        th_string_list=[i.string for i in th_list]
+        th_string_list = [i.string for i in th_list]
         # print(th_string_list)
         #<=星6のデータがあるときのみデータを抽出=>#
         if "初期レアリティ" in th_string_list:
-            a_tag=tr.find("a")
+            a_tag = tr.find("a")
             rarity = [a_tag.string]
         elif "属性" in th_string_list:
-            a_tag=tr.find("a")
+            a_tag = tr.find("a")
             attribution = [a_tag.string]
         else:
-            result=None
+            result = None
     for tag in divs:
         try:
             string_ = tag.get("class").pop(0)
             if string_ in "symphogearxd_gear_table2":
-                table=tag.find_all("tr")
+                table = tag.find_all("tr")
                 break
         except:
             pass
     #<= 行の抽出=>#
     for tr in table:
         th_list = tr.find_all("th")
-        th_string_list=[i.string for i in th_list]
+        th_string_list = [i.string for i in th_list]
         # print(th_string_list)
         #<=星6のデータがあるときのみデータを抽出=>#
         if "星6" in th_string_list:
-            tds=tr.find_all("td")
-            tds=[i.string for i in tds]
+            tds = tr.find_all("td")
+            tds = [i.string for i in tds]
             # print(tds)
             result = rarity + attribution + name_skill + tds
         else:
-            result=None
+            result = None
 
     # print(result)
 
@@ -82,19 +82,19 @@ def extract_data(url):
 def read_text(text_path):
     with open(text_path) as f:
         lines = f.readlines()
-        data=[line.rstrip("\n")for line in lines]
+        data = [line.rstrip("\n")for line in lines]
     # print(data)
     return data
 
 def main():
     #<= パラメータの設定 =>#
-    text_path="./url_list.txt"
-    url_list=read_text(text_path)
-    save_file_path="./symphogear.csv"
+    text_path = "./url_list.txt"
+    url_list = read_text(text_path)
+    save_file_path = "./symphogear.csv"
     save_file = open(save_file_path,"w")
     writer = csv.writer(save_file,lineterminator='\n')
     print("url_length:{}".format(len(url_list)))
-    columns=["初期レアリティ","属性","名前","必殺技","HP","ATK","DEF","SPD","CTR","CTD"]
+    columns = ["初期レアリティ","属性","名前","必殺技","HP","ATK","DEF","SPD","CTR","CTD"]
     writer.writerow(columns)
     for index in range(len(url_list)):
         url = url_list[index]
@@ -110,5 +110,5 @@ def main():
     # print("結果：{}".format(result))
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
